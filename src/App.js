dd8bceafdb6331bb05d7b02267faee9845c994e4
lovechino <quochung13122002@gmail.com>
2024-05-24 00:26:29 +0700

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const[value,SetValue] = useState(0)
+  const fetchData = async ()=>{
+    const res = await axios.get('https://data-school.onrender.com/api/students').then(data=>console.log(data.data)).catch(err=>console.log(err.toJSON().message))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button title='test' onClick={()=>fetchData()}/>
     </div>
   );
 }
